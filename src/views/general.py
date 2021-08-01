e99d03bd1d633e1cebc328691aed6c56b8c42748
Vijayasai Sanagaram <vijayasai10@gmail.com>
2021-08-01 16:28:46 +0530

    Get Tasks from DB

--- src/views/general.py
+++ src/views/general.py
@@ -1,7 +1,7 @@
 import os,sys
 sys.path.append(os.path.abspath('./src/'))
-from src.util.service_util import custom_response
-from flask import request, json, Blueprint
+from src.util.service_util import custom_response, validate_token
+from flask import request, Blueprint, current_app
 
 
 """Blueprint"""
@@ -10,7 +10,7 @@ general_api = Blueprint('general', __name__)
 
 """Load Tasks to the DB API"""
 @general_api.route('/load', methods=['POST'])
-def load_tasks():
+def add_task():
     """
         Note - 
         Load the tasks under the name of the person.
@@ -34,7 +34,30 @@ def get_tasks():
         But it has two keys, one - daily, second - general.
         We are laoding this to only general
     """
+    decoded_token = validate_token(request.headers.get('Authorization', None))
+    db_object = current_app.config["db_connect"]
+    data = db_object.tasks.find_one({"name":decoded_token['email']})
 
-    items = []
+    """
+        Need to check what data is coming
+    """
 
-    return custom_response({"response":{"message":items}},201)
+    return custom_response({"response":{"message":data["general"]}},201)
+
+
+
+
+"""Get Tasks to the DB API"""
+@general_api.route('/update', methods=['PUT'])
+def update_tasks():
+    pass
+
+
+"""Get Tasks to the DB API"""
+@general_api.route('/delete', methods=['DELETE'])
+def delete_tasks():
+    """
+        Note - Check whether to pass it in body or as path parameter
+
+    """
+    pass
