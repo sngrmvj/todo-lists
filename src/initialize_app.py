c619290d5fc8d8386321fe2addbffedaba5e5c3c
Vijayasai Sanagaram <vijayasai10@gmail.com>
2021-08-01 14:52:51 +0530

    Starting of Blueprints

--- src/initialize_app.py
+++ src/initialize_app.py
@@ -4,7 +4,7 @@ from flask_pymongo import PyMongo
 import os,sys
 sys.path.append(os.path.abspath('./src/')) 
 from src.config import app_config 
-
+from src.views.general import general_api
 
 
 def create_app(env_name):
@@ -20,6 +20,7 @@ def create_app(env_name):
 
 
   # Add Blueprints here 
+  app.register_blueprint(general_api, url_prefix='todo/v1/general') 
 
 
   return app
