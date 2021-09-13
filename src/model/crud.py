c2bec5e791d50a9732085055e822757f9845c8a8
Vijayasai Sanagaram <vijayasai10@gmail.com>
2021-09-13 11:15:02 +0530

    crud operations on mongodb

--- src/model/crud.py
+++ src/model/crud.py
@@ -26,8 +26,59 @@ def get_record(query, type):
         
         # Note the query should be a dict
         mydoc = mycol.find(query)
-        for item in mydoc:
-            return item
+        if mydoc:
+            for item in mydoc:
+                return item
+        else:
+            return False
 
     except Exception as error:
         print(f"Error while getting the particular record - {error}")
+
+
+
+def insert_record(query,type):
+    try:
+        myclient = pymongo.MongoClient(os.environ.get('DATABASE_URL'))
+        mydb = myclient[os.environ.get('DATABASE_NAME')]
+        collist = mydb.list_collection_names()
+        if type == 'daily':
+            mycol = mydb[os.environ.get('DAILY_COLLECTION')]
+        elif type == 'general':
+            mycol = mydb[os.environ.get('GENERAL_COLLECTION')]
+        
+        # Note the query should be a dict
+        mydoc = mycol.find(query)
+        if mydoc:
+            for item in mydoc:
+                return item
+        else:
+            return False
+
+    except Exception as error:
+        print(f"Error while getting the particular record - {error}")
+
+
+
+
+
+def update_record(query,new_values,type):
+    try:
+        myclient = pymongo.MongoClient(os.environ.get('DATABASE_URL'))
+        mydb = myclient[os.environ.get('DATABASE_NAME')]
+        collist = mydb.list_collection_names()
+        if type == 'daily':
+            mycol = mydb[os.environ.get('DAILY_COLLECTION')]
+        elif type == 'general':
+            mycol = mydb[os.environ.get('GENERAL_COLLECTION')]
+        
+        # Note the query should be a dict
+        mycol.update_one(query,new_values)
+        for item in mycol.find(query):
+            if item:
+                return True
+        else:
+            return False
+
+    except Exception as error:
+        print(f">>>> Error while updating the particular record - {error}")
