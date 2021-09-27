fa8186d888514b1239de934f193a2d59ce67890e
Vijayasai S <vijayasai10@gmail.com>
2021-09-27 23:16:48 +0530

    Toogling of tasks in general tasks

--- src/views/general.py
+++ src/views/general.py
@@ -51,7 +51,9 @@ def post_general_tasks():
     
     value, booleans, status = verify_user()
     if booleans == False:
-        retun custom_response({"error": value}, status)
+        return custom_response({"error": value}, status)
+    else:
+        username = value
 
     try:
 
@@ -63,17 +65,17 @@ def post_general_tasks():
         task = data['value']['general']['taskItem']
 
         # Get person's record 
-        record = crud.get_record({"name":username},'general')
+        record = crud.get_record({"name":username},type)
         if record == None:
-            insert_query = {"name":username,'active':[task]}
-            new_value = crud.insert_record({"name":username},insert_query,'general')
-            return custom_response({"response":new_value['general']},201)
+            insert_query = {"name":username,'active':[task],'deactive':[]}
+            new_value = crud.insert_record({"name":username},insert_query,type)
+            return custom_response({"response":{"active":new_value['active'],"deactive":new_value['deactive']}},201)
         else:
-            if task not in record['active']:
+            if task not in record['active'] and task not in record['deactive']:
                 record['active'].append(task)
                 new_query = {"$set":{'active':record['active']}}
-                update_value = crud.update_record({"name": username},new_query,'general')
-                return custom_response({"response":update_value['general']},201)
+                update_value = crud.update_record({"name": username},new_query,type)
+                return custom_response({"response":{"active":update_value['active'],"deactive":update_value['deactive']}},201)
             else:
                 return custom_response({"response":"task already exists","warning":True},200)
     except Exception as error:
@@ -90,13 +92,19 @@ def get_general_active_tasks():
 
     value, booleans, status = verify_user()
     if booleans == False:
-        retun custom_response({"error": value}, status)
-        
+        return custom_response({"error": value}, status)
+    else:
+        username = value
+
+
     try:
+        #Attributes
+        type = 'general'
+
         # Get person's record 
-        record = crud.get_record({"name":username},'general')
+        record = crud.get_record({"name":username},type)
         if record is not None:
-            return custom_response({"response":record['general']},201)
+            return custom_response({"response":{"active":record['active'],"deactive":record['deactive']}},201)
     except Exception as error:
         print(f"Exception during the fetch of the general active tasks - {error}")
         return custom_response({"error":f"Exception during the fetch of the general active tasks - {error}"},500)
@@ -104,18 +112,60 @@ def get_general_active_tasks():
 
 
 
+
+""" Toggle items """
+@general_api.route('/toggle',methods=['PUT'])
+def toggle_tasks():
+    value, booleans, status = verify_user()
+    if booleans == False:
+        return custom_response({"error": value}, status)
+    else:
+        username = value
+
+    try:
+        #Attributes
+        type = 'general'
+
+        # request body 
+        data = request.get_json()
+        task = data['value']
+
+        action = data['action']
+
+        # Get person's record 
+        record = crud.get_record({"name":username},type)
+        if record is not None:
+            if action == 'checked':
+                record['active'].remove(task)
+                record['deactive'].append(task)
+            elif action == 'unchecked':
+                record['deactive'].remove(task)
+                record['active'].append(task)
+            else:
+                raise Exception("Action on task is empty ")
+            new_query = {"$set":{'active':record['active'],'deactive':sorted(record['deactive'])}}
+            update_value = crud.update_record({"name": username},new_query,type)
+            return custom_response({"response":{"active":update_value['active'],"deactive":update_value['deactive']}},201)
+    except Exception as error:
+        print(f"Exception during the fetch of the general active tasks - {error}")
+        return custom_response({"error":f"Exception during the fetch of the general active tasks - {error}"},500)
+
+
 # Things to be done
-# checked in active should add to deactive general tasks. viceversa
 # Permanently delete the task. 
 
 
 
 
-# """Get Tasks to the DB API"""
-# @general_api.route('/delete', methods=['DELETE'])
-# def delete_tasks():
-#     """
-#         Note - Check whether to pass it in body or as path parameter
-#         IMPLEMENT KAFKA HERE
-#     """
-#     pass
+"""Get Tasks to the DB API"""
+@general_api.route('/delete_active', methods=['PUT'])
+def delete_active_tasks():
+
+    pass
+
+
+"""Get Tasks to the DB API"""
+@general_api.route('/delete_deactive', methods=['PUT'])
+def delete_deactive_tasks():
+
+    pass
